7667ea141efe776ef569eb9ba46dc466c89b48be
GetDragon <getdragon@hotmail.com>
2018-09-24 14:40:19 -0500

    Ajustes StartUp

--- Angular Code/CoreApp/CoreAppNet/Startup.cs
+++ Angular Code/CoreApp/CoreAppNet/Startup.cs
@@ -34,6 +34,8 @@ namespace CoreAppNet
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
             app.UseMvc();
         }
     }
